acc41876d062f3887bc60186a6410ca3594771c8
Liam Sutton <devliam90@gmail.com>
2020-09-17 10:34:45 +0100

    quiz app data

---
+++ quiz-app/app.js
@@ -0,0 +1,43 @@
+const quizData = [
+  {
+    question: 'How old is Liam?',
+    a: '18',
+    b: '21',
+    c: '28',
+    d: '30',
+    answer: 'd',
+  },
+  {
+    question:
+      'What is the most used programming language of in 2019?',
+    a: 'C',
+    b: 'Java',
+    c: 'Python',
+    d: 'JavaScript',
+    answer: 'd',
+  },
+  {
+    question: 'Who is the president of the USA?',
+    a: 'Barack Obama',
+    b: 'Donald Trump',
+    c: 'JFK',
+    d: 'Boris Johnson',
+    answer: 'b',
+  },
+  {
+    question: 'What does HTML stand for?',
+    a: 'Hello to my legs',
+    b: 'Hypertext makeup Language',
+    c: 'Having the most luck',
+    d: 'Hypertext markup Language',
+    answer: 'd',
+  },
+  {
+    question: 'What year was JavaScript launched?',
+    a: '1990',
+    b: '1995',
+    c: '1997',
+    d: 'none of the above',
+    answer: 'd',
+  },
+];
